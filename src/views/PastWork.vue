fcb94fbf2e90f0593f90cbb8fc363b5483a29626
Aamir Farooq <aamir@farooq.xyz>
2019-07-21 17:53:43 +0500

    Pastwork: more work, add sample image

--- src/views/PastWork.vue
+++ src/views/PastWork.vue
@@ -1,11 +1,20 @@
 <template>
     <div class="work">
-        
+        <HeroIntro title="My Previous Work" subtitle="A showcase of my past designs, projects" kind="is-info is-bold" />
+        <div class="container">
+            <WorkObjects />
+        </div>
     </div>
 </template>
 
 <script>
+    import HeroIntro from '@/components/HeroIntro'
+    import WorkObjects from '@/components/WorkObjects'
+
 export default {
-    name: 'PastWork'
+    name: 'PastWork',
+    components: {
+        HeroIntro, WorkObjects
+    }
 }
 </script>
