c8dd4588d0891ca22afb1f84da65ff8d945c8ce8
Aamir Farooq <aamir@farooq.xyz>
2019-07-21 17:53:08 +0500

    PastWork: add list of work objects component

---
+++ src/components/WorkObjects.vue
@@ -0,0 +1,30 @@
+<template>
+    <section class="section">
+        <WorkObject v-for="work in workData" :key="work.id" :work="work" />
+    </section>
+</template>
+
+<script>
+import WorkObject from '@/components/WorkObject';
+import uuid from 'uuid';
+
+export default {
+    name: "WorkObjects",
+    components: {
+        WorkObject
+    },
+    data() {
+        return {
+            workData: [
+                {
+                    "id": uuid.v4(),
+                    "name": "Zira Dashboard",
+                    "description": "Esketiit",
+                    "demo": "https://zira.slim.ovh",
+                    "preview": require('@/assets/works/zira.png')
+                }
+            ]
+        }
+    }
+}
+</script>
