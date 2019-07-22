df9bf181d2b27f92c21847a1c0ef6b924e1e96e1
Aamir Farooq <aamir@farooq.xyz>
2019-07-22 16:35:47 +0500

    Remove separate About page, move to home

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,9 +1,6 @@
 <template>
-	<div class="about">
+	<div id="about">
 		<HeroIntro title="About me" subtitle="Learn a little about who I am" kind="is-info is-bold" />
-		<section class="section">
-			<Contact />
-		</section>
 		<section class="section">
 			<Tiles />
 		</section>
@@ -13,14 +10,12 @@
 <script>
 	import HeroIntro from '@/components/HeroIntro';
 	import Tiles from '@/components/Tiles';
-	import Contact from '@/components/Contact';
 
 	export default {
 		name: 'about',
 		components: {
 			HeroIntro,
 			Tiles,
-			Contact
 		}
 	} 
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <HeroHome />
+    <About />
   </div>
 </template>
 
 <script>
 import HeroHome from '@/components/HeroHome';
+import About from '@/components/About';
 
 export default {
   name: 'home',
   components: {
-    HeroHome
+    HeroHome,
+    About
   }
 }
 </script>
