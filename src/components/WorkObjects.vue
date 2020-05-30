76ae2db301b1c39fbe126438aed9f4320a2c95c6
Aamir Farooq <aamir@farooq.xyz>
2020-05-30 12:55:45 +0200

    More image updates

--- src/components/WorkObjects.vue
+++ src/components/WorkObjects.vue
@@ -95,7 +95,7 @@ export default {
 					id: uuid.v4(),
 					name: "Material Admin",
 					type: "HTML Template",
-					description: `This is a simple template for an administrative panel that is based on the <a href="http://materializecss.com">materializecss.com</a> framework to give the site a beautiful look, adhering to the principles of Google&#39;s Material design language. This template is currently in use as the moderator control panel at <a href="https://opdessertstorm.com">opdessertstorm.com</a>, a game with over <strong>32,000 accounts</strong>.`,
+					description: `An adminisrative panel template based on the <a href="http://materializecss.com">materializecss.com</a> CSS framework. This template is currently in use as the moderator control panel at <a href="https://opdessertstorm.com">opdessertstorm.com</a>, a game with over <strong>32,000 accounts</strong>. This template was also used in my Runner Assist project, and it was adapted for <a href="https://zira.ovh">Zira's</a> dashboard <a href="https://projects.aamirfarooq.dev/zira/">(demo here</a>, screenshots in gallery).`,
 					demo: "https://projects.aamirfarooq.dev/material-admin/",
 					source: "https://github.com/SlimShadyIAm/material-admin/",
 					preview: this.importAll(
@@ -106,26 +106,26 @@ export default {
 						)
 					)
 				},
-				{
-					id: uuid.v4(),
-					name: "Zira Dashboard",
-					type: "HTML template",
-					description: `This template, based off of my <a href="https://github.com/SlimShadyIAm/material-admin">Material Admin</a> template, was created as a static HTML mockup for an administrative dashboard for <a href="https://zira.ovh">Zira</a>, a Discord bot with almost <b>10 million users</b> and is in use in 57,000 guilds. The dashboard serves as a graphical way to control the bot&#39;s configuration.`,
-					demo: "https://projects.aamirfarooq.dev/zira/",
-					source: "https://github.com/SlimShadyIAm/zira-dashboard/",
-					preview: this.importAll(
-						require.context(
-							"@/assets/img/works/zira",
-							false,
-							/\.(png|jpe?g|svg)$/
-						)
-					)
-				},
+				// {
+				// 	id: uuid.v4(),
+				// 	name: "Zira Dashboard",
+				// 	type: "HTML template",
+				// 	description: `This template, based off of my <a href="https://github.com/SlimShadyIAm/material-admin">Material Admin</a> template, was created as a static HTML mockup for an administrative dashboard for <a href="https://zira.ovh">Zira</a>, a Discord bot with almost <b>10 million users</b> and is in use in 57,000 guilds. The dashboard serves as a graphical way to control the bot&#39;s configuration.`,
+				// 	demo: "https://projects.aamirfarooq.dev/zira/",
+				// 	source: "https://github.com/SlimShadyIAm/zira-dashboard/",
+				// 	preview: this.importAll(
+				// 		require.context(
+				// 			"@/assets/img/works/zira",
+				// 			false,
+				// 			/\.(png|jpe?g|svg)$/
+				// 		)
+				// 	)
+				// },
 				{
 					id: uuid.v4(),
 					name: "Froebel's Model UN 2016",
 					type: "HTML",
-					description: `This website was created to be used as the official site for the Model UN of Froebel's International School, the high school I attended. Here, students were able to find information regarding the host team staff, scheduling as well as how to register to participate. The final version, tweaked by Rockville, can be found at <a href="https://froebels.edu.pk/fromun">froebels.edu.pk/fromun.</a>, though it has since been modified by students to reflect information from newer years.`,
+					description: `This website was created to be used as the informational site for the Model UN of Froebel's International School, the high school I attended. A version of this is still being used today: <a href="https://froebels.edu.pk/fromun">froebels.edu.pk/fromun</a>.`,
 					demo: "https://projects.aamirfarooq.dev/fromun-site",
 					source: "https://github.com/SlimShadyIAm/fromun-site",
 					preview: this.importAll(
